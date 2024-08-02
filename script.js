971d89aef48c49f1c6fcdcd5441f0a1fe613930d
nafiuishaaq <nafiuig@gmail.com>
2024-08-02 16:25:29 +0100

    fully responsive hamburger menu

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const menu = document.getElementById("hamburger");
 const menulist = document.getElementById("list");
 const menuclose = document.getElementById("close");
+const caret = document.querySelectorAll(".fa-caret-down");
 
 menu.addEventListener("click", function () {
   menulist.classList.toggle("active");
@@ -9,6 +10,8 @@ menu.addEventListener("click", function () {
   if (menulist.classList.contains("active")) {
     menu.classList.remove("fa-close");
     menu.classList.add("fa-bars");
+    caret.classList.remove("fa-caret-down");
+    caret.classList.add("fa-caret-right");
   } else {
     menu.classList.remove("fa-bars");
     menu.classList.add("fa-close");
