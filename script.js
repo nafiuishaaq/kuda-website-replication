53a7b672ee968b7d77f01fec05cc3560c71ef68e
nafiuishaaq <nafiuig@gmail.com>
2024-08-02 15:17:07 +0100

    some hamburger menu

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 const menu = document.getElementById("hamburger");
 const menulist = document.getElementById("list");
+const menuclose = document.getElementById("close");
 
 menu.addEventListener("click", function () {
   menulist.classList.remove("active");
-  menu.style.display = none;
+  menu.classList.remove("fa-bars");
+  menu.classList.add("fa-close");
+
+  // closemenu();
 });
+
+// function closemenu() {
+//   if (menu.classList === "fa-bars") {
+//     menu.classList.remove("fa-bars");
+//   }
+// }
